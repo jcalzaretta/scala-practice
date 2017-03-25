964b9942be856dbd165a0f9ce8d5f6441677dd0b
Jack Calzaretta <jackc@groupon.com>
2017-03-25 12:39:55 -0500

    remove extraneous args to go method.  N can be accessed directly from fib.

--- chp2/exercises/fibonacci.scala
+++ chp2/exercises/fibonacci.scala
@@ -1,10 +1,10 @@
 object Fibonacci {
   def fib(n: Int): Int = {
-    def go(one: Int, two: Int, acc: Int, n: Int): Int =
+    def go(one: Int, two: Int, acc: Int): Int =
       if(acc == n) one + two
-      else  go(two, one + two, acc + 1, n)
+      else  go(two, one + two, acc + 1)
 
-    go(0, 1, 2, n)
+    go(0, 1, 2)
   }
 
   def main(args: Array[String]): Unit =
